1c5dc3e20ca31874e62cdf3c5c62c0c1de474957
aliko123456 <168002967+aliko123456@users.noreply.github.com>
2024-06-11 19:17:05 +0200

    Update App.svelte

--- App.svelte
+++ App.svelte
@@ -2,20 +2,10 @@
   import Experience from './Experience.svelte';
 </script>
 
-<main class="bg-gray-100">
-  <div class="container mx-auto flex max-w-4xl p-5">
-    <aside class="w-1/4 bg-blue-700 p-5 text-white">
-      <h2 class="text-2xl">Pli Poçak</h2>
-      <img src="A.jpg" alt="Photo de Ali Koçak" class="w-48 h-auto my-4">
-      <p class="text-sm mb-4">"As a student with a passion for corporate finance, I have solid experience in financial auditing at KPMG and as a junior consultant at Valeo"</p>
-      <ul class="list-none p-0">
-        <li class="mb-2"><a href="mailto:ali.kocak@essec.edu" class="text-white text-sm">ali.kocak@essec.edu</a></li>
-        <li><a href="https://www.linkedin.com/in/ali-ko%C3%A7ak-2b07b618a/" class="text-white text-sm">https://www.linkedin.com/in/ali-ko%C3%A7ak-2b07b618a/</a></li>
-      </ul>
-    </aside>
-
-    <main class="w-3/4 p-5">
-      <Experience />
-    </main>
-  </div>
+<main>
+  <Experience />
 </main>
+
+<style>
+  /* Ajoutez ici vos styles globaux si nécessaire */
+</style>
