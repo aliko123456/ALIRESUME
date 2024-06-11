70f2e790c0af58b1eaf08cd66653553e415e360d
aliko123456 <168002967+aliko123456@users.noreply.github.com>
2024-06-11 18:40:04 +0200

    Create Education.svelte

---
+++ Education.svelte
@@ -0,0 +1,12 @@
+<!-- Education.svelte -->
+<script>
+  let isMoved = false;
+
+  function moveLeft() {
+    isMoved = !isMoved;
+  }
+</script>
+
+<div class="{isMoved ? 'translate-x-10' : ''}" on:click={moveLeft}>
+  <!-- Your education content here -->
+</div>
