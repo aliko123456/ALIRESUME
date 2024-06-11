5f2ca5c514fa150c6beef7380049bf9f054baa4d
aliko123456 <168002967+aliko123456@users.noreply.github.com>
2024-06-11 18:39:22 +0200

    Update and rename Svelt to Experience.svelte

---
+++ Experience.svelte
@@ -0,0 +1,12 @@
+<!-- Experience.svelte -->
+<script>
+  let isRed = false;
+
+  function toggleColor() {
+    isRed = !isRed;
+  }
+</script>
+
+<div class="{isRed ? 'text-red-500' : ''}" on:click={toggleColor}>
+  <!-- Your experience content here -->
+</div>
